9c49cf4c9f1913a35a36b8e315b729cc72a3c881
Leonardo Losso <93181690+LeonardoLosso@users.noreply.github.com>
2022-11-28 10:33:03 -0300

    Update Program.cs
    CORS policy default

--- WebScraipingHttpsAPI/WebScraipingHttpsAPI/Program.cs
+++ WebScraipingHttpsAPI/WebScraipingHttpsAPI/Program.cs
@@ -1,12 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+        });
+});
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
